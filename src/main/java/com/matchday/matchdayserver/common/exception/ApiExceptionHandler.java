dbc646148a974728d2932bd6ad89f76e14d270f0
rookedsysc <rookedsysc36@gmail.com>
2025-04-12 20:46:45 +0900

    fix: api exception handler에서 status code 변경 안되던 부분 수정 및 Exception 임시로 핸들링 하도록 추가

--- src/main/java/com/matchday/matchdayserver/common/exception/ApiExceptionHandler.java
+++ src/main/java/com/matchday/matchdayserver/common/exception/ApiExceptionHandler.java
@@ -1,14 +1,29 @@
 package com.matchday.matchdayserver.common.exception;
 
 import com.matchday.matchdayserver.common.response.ApiResponse;
+import com.matchday.matchdayserver.common.response.DefaultStatus;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
+@Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
     @ExceptionHandler(ApiException.class)
-    public ApiResponse<ApiExceptionInterface> handleApiException(Exception ex, ApiException apiException) {
-        return ApiResponse.error(apiException.getStatus());
+    public ResponseEntity<ApiResponse<ApiExceptionInterface>> handleApiException(Exception ex, ApiException apiException) {
+        return ResponseEntity
+            .status(apiException.getStatus().getHttpStatusCode())
+            .body(ApiResponse.error(apiException.getStatus()));
+    }
+
+    @ExceptionHandler(Exception.class)
+    public ResponseEntity<ApiResponse<ApiExceptionInterface>> handleException(Exception ex) {
+        log.error("Internal server error", ex);
+        return ResponseEntity
+            .status(HttpStatus.INTERNAL_SERVER_ERROR)
+            .body(ApiResponse.error(DefaultStatus.SERVER_ERROR));
     }
 }
