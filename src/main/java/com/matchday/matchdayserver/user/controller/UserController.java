79a42d202b9c6e0591dfb04483913eeb42dd684c
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-12 17:59:10 +0900

    feat(user): User엔티티 생성,유저 생성 임시 Controller 구현,User 생성요청 DTO 생성

---
+++ src/main/java/com/matchday/matchdayserver/user/controller/UserController.java
@@ -0,0 +1,25 @@
+package com.matchday.matchdayserver.user.controller;
+
+import com.matchday.matchdayserver.common.response.ApiResponse;
+import com.matchday.matchdayserver.user.model.Entity.User;
+import com.matchday.matchdayserver.user.model.dto.request.UserCreateRequest;
+import com.matchday.matchdayserver.user.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/v1/user")
+@RequiredArgsConstructor
+public class UserController {
+
+    private final UserRepository userRepository;
+
+    @PostMapping("/create")
+    public ApiResponse<?> createUser(@RequestBody UserCreateRequest request) {
+        //***임시*** 원래는 서비스단에서 처리할 내용
+        User user = new User(request.getName());
+        userRepository.save(user);
+
+        return ApiResponse.ok("유저생성완료");
+    }
+}
---
+++ src/main/java/com/matchday/matchdayserver/user/model/Entity/User.java
@@ -0,0 +1,27 @@
+package com.matchday.matchdayserver.user.model.Entity;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, length = 30)
+    private String name;
+
+    @Builder
+    public User (String name) {
+        this.name = name;
+    }
+
+    public void updateName(String name) {
+        this.name = name;
+    }
+}
