9b26e040d2362e3f8f1c2a9481c2de6f0eac4f58
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-12 23:46:50 +0900

    "refactor(user): 엔티티 unique 제약 조건 추가"

--- src/main/java/com/matchday/matchdayserver/user/model/Entity/User.java
+++ src/main/java/com/matchday/matchdayserver/user/model/Entity/User.java
@@ -13,7 +13,7 @@ public class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    @Column(nullable = false, length = 30)
+    @Column(nullable = false, length = 30, unique = true)
     private String name;
 
     @Builder
