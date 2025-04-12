9dc71e7cbd9742794c2a4a6bd137546dfa525173
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-13 00:01:25 +0900

    feat(common): UserStatus정의

---
+++ src/main/java/com/matchday/matchdayserver/common/response/UserStatus.java
@@ -0,0 +1,32 @@
+package com.matchday.matchdayserver.common.response;
+
+import java.io.Serializable;
+
+public enum UserStatus implements StatusInterface {
+    DUPLICATE_NAME(400,4001, "이미 존재하는 유저 이름");
+
+    private final int httpStatusCode;
+    private final int customStatusCode;
+    private final String description;
+
+    UserStatus(int httpStatusCode, int customStatusCode, String description) {
+        this.httpStatusCode = httpStatusCode;
+        this.customStatusCode = customStatusCode;
+        this.description = description;
+    }
+
+    @Override
+    public int getHttpStatusCode() {
+        return this.httpStatusCode;
+    }
+
+    @Override
+    public int getCustomStatusCode() {
+        return this.customStatusCode;
+    }
+
+    @Override
+    public String getDescription() {
+        return this.description;
+    }
+}
