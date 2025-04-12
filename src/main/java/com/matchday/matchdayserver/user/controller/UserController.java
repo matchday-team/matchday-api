ab878b525dd920d6a7ee48cd527ddbf00f4c07e5
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-12 18:12:23 +0900

    docs(user): createUser 스웨거 설명 추가

--- src/main/java/com/matchday/matchdayserver/user/controller/UserController.java
+++ src/main/java/com/matchday/matchdayserver/user/controller/UserController.java
@@ -4,6 +4,7 @@ import com.matchday.matchdayserver.common.response.ApiResponse;
 import com.matchday.matchdayserver.user.model.Entity.User;
 import com.matchday.matchdayserver.user.model.dto.request.UserCreateRequest;
 import com.matchday.matchdayserver.user.repository.UserRepository;
+import io.swagger.v3.oas.annotations.Operation;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -14,6 +15,7 @@ public class UserController {
 
     private final UserRepository userRepository;
 
+    @Operation(summary = "임시 유저 생성")
     @PostMapping("/create")
     public ApiResponse<?> createUser(@RequestBody UserCreateRequest request) {
         //***임시*** 원래는 서비스단에서 처리할 내용
