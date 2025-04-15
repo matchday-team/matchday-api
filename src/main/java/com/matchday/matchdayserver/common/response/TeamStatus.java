1cdaa121c6524d2cf849e78713dc84f86bca9270
Juri-Jang <jjr84@dgu.ac.kr>
2025-04-15 20:28:02 +0900

    refactor(team): TeamStatus customStatusCode 변경 (4000번대 -> 5000번대)

--- src/main/java/com/matchday/matchdayserver/common/response/TeamStatus.java
+++ src/main/java/com/matchday/matchdayserver/common/response/TeamStatus.java
@@ -1,9 +1,9 @@
 package com.matchday.matchdayserver.common.response;
 
 public enum TeamStatus implements StatusInterface {
-    DUPLICATE_TEAMNAME(400,4001, "이미 존재하는 팀 이름"),
-    NOTFOUND_TEAM(400,4002, "해당 팀이 존재하지 않습니다"),
-    ALREADY_JOINED_USER(400,4003, "팀에 이미 유저가 소속되어있음");
+    DUPLICATE_TEAMNAME(400,5001, "이미 존재하는 팀 이름"),
+    NOTFOUND_TEAM(400,5002, "해당 팀이 존재하지 않습니다"),
+    ALREADY_JOINED_USER(400,5003, "팀에 이미 유저가 소속되어있음");
 
     private final int httpStatusCode;
     private final int customStatusCode;
