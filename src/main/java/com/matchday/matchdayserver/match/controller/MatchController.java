158d64bc3014ba13509b4dd1942d7d190a0e1b21
Juri-Jang <jjr84@dgu.ac.kr>
2025-04-17 01:10:56 +0900

    refactor(match): Swagger Tag추가

--- src/main/java/com/matchday/matchdayserver/match/controller/MatchController.java
+++ src/main/java/com/matchday/matchdayserver/match/controller/MatchController.java
@@ -4,12 +4,14 @@ import com.matchday.matchdayserver.common.response.ApiResponse;
 import com.matchday.matchdayserver.match.model.dto.request.MatchCreateRequest;
 import com.matchday.matchdayserver.match.service.MatchService;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+@Tag(name = "matches", description = "매치 관련 API")
 @RestController
 @RequestMapping("/api/v1/matches")
 @RequiredArgsConstructor
