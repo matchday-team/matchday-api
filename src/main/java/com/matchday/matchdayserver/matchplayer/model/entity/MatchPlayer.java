9a81ff261730919564562ff1ee60507aaf320d7d
Juri-Jang <jjr84@dgu.ac.kr>
2025-04-17 22:22:14 +0900

    feat(matchplayer): Match Player Entity 생성

---
+++ src/main/java/com/matchday/matchdayserver/matchplayer/model/entity/MatchPlayer.java
@@ -0,0 +1,33 @@
+package com.matchday.matchdayserver.matchplayer.model.entity;
+
+import com.matchday.matchdayserver.match.model.entity.Match;
+import com.matchday.matchdayserver.user.model.entity.User;
+import jakarta.persistence.*;
+import lombok.Builder;
+
+@Entity
+public class MatchPlayer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "match_id", nullable = false)
+    private Match match;
+
+    @ManyToOne(fetch = FetchType.LAZY)
+    @JoinColumn(name = "user_id", nullable = false)
+    private User user;
+
+    @Enumerated(EnumType.STRING)
+    @Column(nullable = false)
+    private Role role;
+
+    @Column(name = "match_position", nullable = false)
+    private String matchPosition;
+
+    public enum Role {
+        START_PLAYER, SUB_PLAYER
+    }
+
+}
