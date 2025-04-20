d08c23aba44c7f2c89820c6d1121ad02ef3134f8
roky <rookedsysc36@gmail.com>
2025-04-20 21:58:59 +0900

    style: MatchEventRequest 필드 전부 fina로 변경

--- src/main/java/com/matchday/matchdayserver/matchevent/model/dto/MatchEventRequest.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/model/dto/MatchEventRequest.java
@@ -8,15 +8,15 @@ import lombok.Getter;
 
 @Getter
 @Builder
-@Schema(description = "경기 이벤트 요청 DTO")
 @AllArgsConstructor
+@Schema(description = "경기 이벤트 요청 DTO")
 public class MatchEventRequest {
   @Schema(description = "경기 ID", example = "1")
-  private Long matchId;
+  private final Long matchId;
 
   @Schema(description = "이벤트 타입", example = "GOAL")
-  private MatchEventType eventType;
+  private final MatchEventType eventType;
 
   @Schema(description = "이벤트 설명", example = "좋은 골이었습니다!")
-  private String description;
+  private final String description;
 }
