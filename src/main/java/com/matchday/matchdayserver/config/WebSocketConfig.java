b24118b679ab830f39b4890cfa5b5db9e9b1f86e
roky <rookedsysc36@gmail.com>
2025-04-20 19:31:35 +0900

    feat: feat: WebSocket Config 추가

---
+++ src/main/java/com/matchday/matchdayserver/config/WebSocketConfig.java
@@ -0,0 +1,26 @@
+package com.matchday.matchdayserver.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.messaging.simp.config.MessageBrokerRegistry;
+import org.springframework.web.socket.config.annotation.EnableWebSocketMessageBroker;
+import org.springframework.web.socket.config.annotation.StompEndpointRegistry;
+import org.springframework.web.socket.config.annotation.WebSocketMessageBrokerConfigurer;
+
+@Configuration
+@EnableWebSocketMessageBroker
+public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
+
+    @Override
+    public void configureMessageBroker(MessageBrokerRegistry config) {
+        config.enableSimpleBroker("/topic");
+        config.setApplicationDestinationPrefixes("/app");
+    }
+
+    @Override
+    public void registerStompEndpoints(StompEndpointRegistry registry) {
+        registry.addEndpoint("/ws")
+                // TODO: 추후 프론트엔드 주소 수정 필요
+                .setAllowedOriginPatterns("*")
+                .withSockJS();
+    }
+}
