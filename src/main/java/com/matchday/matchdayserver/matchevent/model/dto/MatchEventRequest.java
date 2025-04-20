efa21328d3cdd03282bb48eb7a894d2ab6451333
roky <rookedsysc36@gmail.com>
2025-04-20 21:56:30 +0900

    delete(MatchEventRequest): userId 삭제 (Message에서 Token에서 빼내올 예정)

--- src/main/java/com/matchday/matchdayserver/matchevent/model/dto/MatchEventRequest.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/model/dto/MatchEventRequest.java
@@ -14,13 +14,9 @@ public class MatchEventRequest {
   @Schema(description = "경기 ID", example = "1")
   private Long matchId;
 
-  @Schema(description = "사용자 ID", example = "1")
-  private Long userId;
-
   @Schema(description = "이벤트 타입", example = "GOAL")
   private MatchEventType eventType;
 
   @Schema(description = "이벤트 설명", example = "좋은 골이었습니다!")
   private String description;
 }
-
