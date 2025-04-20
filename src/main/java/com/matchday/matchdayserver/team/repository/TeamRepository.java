3324b0c6876cb0143c9bfe0f81bdb6de365706b7
roky <rookedsysc36@gmail.com>
2025-04-20 19:34:08 +0900

    feat(TeamRepository): findByMatchIdAndUserId Method 추가

--- src/main/java/com/matchday/matchdayserver/team/repository/TeamRepository.java
+++ src/main/java/com/matchday/matchdayserver/team/repository/TeamRepository.java
@@ -6,13 +6,16 @@ import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface TeamRepository extends JpaRepository<Team, Long> {
     boolean existsByName(String name);
 
-    //ngram 기반 검색
+    // ngram 기반 검색
     @Query(value = "SELECT * FROM team WHERE MATCH(name) AGAINST(:keyword IN NATURAL LANGUAGE MODE)", nativeQuery = true)
     List<Team> searchByKeyword(@Param("keyword") String keyword);
 
     List<Team> findAllBy();
+
+    Optional<Team> findByMatchIdAndUserId(Long matchId, Long userId);
 }
