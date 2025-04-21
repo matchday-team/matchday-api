b014ebdde3d3ee9c5ee3abd11722563b29fd4ec2
Juri-Jang <jjr84@dgu.ac.kr>
2025-04-22 01:25:10 +0900

    refactor(Team): Team Status 팀 컬러 부분 삭제

--- src/main/java/com/matchday/matchdayserver/common/response/TeamStatus.java
+++ src/main/java/com/matchday/matchdayserver/common/response/TeamStatus.java
@@ -1,10 +1,9 @@
 package com.matchday.matchdayserver.common.response;
 
 public enum TeamStatus implements StatusInterface {
-    DUPLICATE_TEAMNAME(400,5001, "이미 존재하는 팀 이름"),
+    DUPLICATE_TEAMNAME(400, 5001, "이미 존재하는 팀 이름"),
     NOTFOUND_TEAM(400,5002, "해당 팀이 존재하지 않습니다"),
-    ALREADY_JOINED_USER(400,5003, "팀에 이미 유저가 소속되어있음"),
-    REQUIRED_TEAM_COLOR(400,5004, "팀 컬러를 입력하세요");
+    ALREADY_JOINED_USER(400,5003, "팀에 이미 유저가 소속되어있음");
 
     private final int httpStatusCode;
     private final int customStatusCode;
