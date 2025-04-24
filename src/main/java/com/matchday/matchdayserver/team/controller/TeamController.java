625ce182bad28c08923e84bd592cf77e01407cbf
Juri-Jang <jjr84@dgu.ac.kr>
2025-04-24 21:57:26 +0900

    refactor(Team): TeamSearchListResponse dto삭제

--- src/main/java/com/matchday/matchdayserver/team/controller/TeamController.java
+++ src/main/java/com/matchday/matchdayserver/team/controller/TeamController.java
@@ -3,7 +3,6 @@ package com.matchday.matchdayserver.team.controller;
 import com.matchday.matchdayserver.common.response.ApiResponse;
 import com.matchday.matchdayserver.team.model.dto.request.TeamCreateRequest;
 import com.matchday.matchdayserver.team.model.dto.response.TeamResponse;
-import com.matchday.matchdayserver.team.model.dto.response.TeamSearchListResponse;
 import com.matchday.matchdayserver.team.model.dto.response.TeamMemberListResponse;
 import com.matchday.matchdayserver.team.model.dto.response.TeamSearchResponse;
 import com.matchday.matchdayserver.team.service.TeamService;
@@ -32,18 +31,16 @@ public class TeamController {
 
     @Operation(summary = "팀 검색", description = "키워드로 팀 검색하는 API입니다.")
     @GetMapping("/search")
-    public ApiResponse<TeamSearchListResponse> searchTeams(@RequestParam String keyword) {
+    public ApiResponse<List<TeamSearchResponse>> searchTeams(@RequestParam String keyword) {
         List<TeamSearchResponse> teamList = teamService.searchTeams(keyword);
-        TeamSearchListResponse response = new TeamSearchListResponse(teamList);
-        return ApiResponse.ok(response);
+        return ApiResponse.ok(teamList);
     }
 
     @Operation(summary = "팀 목록 조회", description = "전체 팀 조회 API입니다")
-    @GetMapping()
-    public ApiResponse<TeamSearchListResponse> getTeamList() {
+    @GetMapping
+    public ApiResponse<List<TeamSearchResponse>> getTeamList() {
         List<TeamSearchResponse> teamList = teamService.getAllTeams();
-        TeamSearchListResponse response = new TeamSearchListResponse(teamList);
-        return ApiResponse.ok(response);
+        return ApiResponse.ok(teamList);
     }
 
     @Operation(summary = "팀 정보 조회", description = "{timeId]의 정보 조회 API입니다.")
