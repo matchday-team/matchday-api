ece02f027647e1a5c2457ae310866ec9c5ca5014
roky <rookedsysc36@gmail.com>
2025-04-26 20:48:31 +0900

    feat(ApiExceptionHandler): 로깅 추가

--- src/main/java/com/matchday/matchdayserver/common/exception/ApiExceptionHandler.java
+++ src/main/java/com/matchday/matchdayserver/common/exception/ApiExceptionHandler.java
@@ -2,6 +2,7 @@ package com.matchday.matchdayserver.common.exception;
 
 import com.matchday.matchdayserver.common.response.ApiResponse;
 import com.matchday.matchdayserver.common.response.DefaultStatus;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.HttpStatus;
 import org.springframework.validation.FieldError;
 import org.springframework.web.bind.MethodArgumentNotValidException;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 
 import java.util.List;
 
+@Slf4j
 @RestControllerAdvice
 public class ApiExceptionHandler {
 
@@ -35,6 +37,7 @@ public class ApiExceptionHandler {
     @ExceptionHandler(Exception.class)
     @ResponseStatus(HttpStatus.INTERNAL_SERVER_ERROR)
     public ApiResponse<String> handleAllExceptions(Exception ex) {
+      log.error("예외 발생: 타입=[{}], 메시지=[{}]", ex.getClass().getName(), ex.getMessage(), ex);
       return ApiResponse.error(DefaultStatus.UNKNOWN_ERROR);
     }
 }
