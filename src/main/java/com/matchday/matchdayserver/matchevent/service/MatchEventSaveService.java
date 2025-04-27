a498f8305d3b76ccd434c90685ff50d4d37e5c3a
roky <rookedsysc36@gmail.com>
2025-04-27 15:24:22 +0900

    feat(saveMatchEvent): request validation 추가

--- src/main/java/com/matchday/matchdayserver/matchevent/service/MatchEventSaveService.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/service/MatchEventSaveService.java
@@ -2,6 +2,7 @@ package com.matchday.matchdayserver.matchevent.service;
 
 import com.matchday.matchdayserver.common.exception.ApiException;
 import com.matchday.matchdayserver.common.model.Message;
+import com.matchday.matchdayserver.common.response.DefaultStatus;
 import com.matchday.matchdayserver.common.response.MatchStatus;
 import com.matchday.matchdayserver.common.response.TeamStatus;
 import com.matchday.matchdayserver.common.response.UserStatus;
@@ -13,6 +14,9 @@ import com.matchday.matchdayserver.matchevent.model.entity.MatchEvent;
 import com.matchday.matchdayserver.user.model.entity.User;
 
 import jakarta.transaction.Transactional;
+
+import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.messaging.simp.SimpMessagingTemplate;
 import org.springframework.stereotype.Service;
@@ -36,6 +40,7 @@ public class MatchEventSaveService {
   private final SimpMessagingTemplate messagingTemplate;
 
   public void saveMatchEvent(Long matchId, Message<MatchEventRequest> request) {
+    validateRequest(matchId, request);
     validateAuthUser(matchId, request.getToken());
 
     MatchUser matchUser = matchUserRepository
@@ -46,8 +51,7 @@ public class MatchEventSaveService {
     Match match = matchUser.getMatch();
 
     Team team = teamRepository.findByMatchIdAndUserId(matchId, user.getId()).orElseThrow(
-        () -> new ApiException(TeamStatus.NOTFOUND_TEAM)
-    );
+        () -> new ApiException(TeamStatus.NOTFOUND_TEAM));
 
     MatchEvent matchEvent = MatchEventMapper.toEntity(request.getData(), match, user);
     matchEventRepository.save(matchEvent);
@@ -63,8 +67,26 @@ public class MatchEventSaveService {
         .findByMatchIdAndUserIdWithFetch(matchId, authId)
         .orElseThrow(() -> new ApiException(UserStatus.NOTFOUND_USER));
 
-    if(!authUser.getMatch().getId().equals(matchId)) {
+    if (!authUser.getMatch().getId().equals(matchId)) {
       throw new ApiException(MatchStatus.NOT_PARTICIPATING_PLAYER);
+    }
+  }
+
+  private void validateRequest(Long matchId, Message<MatchEventRequest> request) {
+    List<String> errorMessages = new ArrayList<>();
+
+    if (request.getData().getUserId() == null) {
+      errorMessages.add("userId는 필수 입력 값입니다.");
+    }
+
+    if (request.getData().getUserId() != matchId) {
+      errorMessages.add("userId는 필수 입력 값입니다.");
+    }
+
+    if (errorMessages.size() > 0) {
+      DefaultStatus defaultStatus = DefaultStatus.BAD_REQUEST;
+      defaultStatus.setCustomDescription(String.join("\n", errorMessages));
+      throw new ApiException(defaultStatus);
     }
   }
 }
