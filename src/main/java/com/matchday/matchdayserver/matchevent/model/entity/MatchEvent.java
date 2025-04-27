7146e30ad70a8d13ba110063714704e48270a234
roky <rookedsysc36@gmail.com>
2025-04-27 17:35:19 +0900

    feat(MatchEvent): copyWith 메서드 생성

--- src/main/java/com/matchday/matchdayserver/matchevent/model/entity/MatchEvent.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/model/entity/MatchEvent.java
@@ -9,6 +9,7 @@ import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
+import lombok.Setter;
 
 import org.hibernate.annotations.CreationTimestamp;
 
@@ -29,18 +30,28 @@ public class MatchEvent {
     @CreationTimestamp
     private LocalDateTime eventTime;
 
+    @Setter
     @Enumerated(EnumType.STRING)
-    @Column(nullable = false , length = 20) //20글자 넘어가는 ENUM 생기면 수정해줘야함
+    @Column(nullable = false, length = 20) // 20글자 넘어가는 ENUM 생기면 수정해줘야함
     private MatchEventType eventType;
 
     @Column(length = 400)
     private String description; // 메모
 
-    @ManyToOne(fetch = FetchType.LAZY, optional = false) //EAGER 로딩이 필요하다면 변경하시오
-    @JoinColumn(name = "match_id", nullable = false) //명시적으로 외래키 명 지정
+    @ManyToOne(fetch = FetchType.LAZY, optional = false) // EAGER 로딩이 필요하다면 변경하시오
+    @JoinColumn(name = "match_id", nullable = false) // 명시적으로 외래키 명 지정
     private Match match;
 
     @ManyToOne(fetch = FetchType.LAZY, optional = false)
     @JoinColumn(name = "user_id", nullable = false)
     private User user;
+
+    public MatchEvent copyWith(MatchEventType eventType) {
+        return MatchEvent.builder()
+                .eventType(eventType)
+                .description(this.description)
+                .match(this.match)
+                .user(this.user)
+                .build();
+    }
 }
