e7d9ec7c58391c2f35b1d710d4e72cd05725c746
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-29 15:35:23 +0900

    feat(matchEvent): 경기에서 선수 기록 조회하는 메소드 생성

---
+++ src/main/java/com/matchday/matchdayserver/matchevent/service/MatchEventQueryService.java
@@ -0,0 +1,48 @@
+package com.matchday.matchdayserver.matchevent.service;
+
+import com.matchday.matchdayserver.matchevent.model.dto.EventTypeCount;
+import com.matchday.matchdayserver.matchevent.repository.MatchEventRepository;
+import com.matchday.matchdayserver.matchuser.model.dto.MatchUserEventStat;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class MatchEventQueryService {
+
+  private MatchEventRepository matchEventRepository;
+  /**
+   * MatchEventStrategy
+   * <p>
+   * 특정 경기에서 특정 선수에 대한 득점,어시스트,파울 개수를 조회합니다
+   * <p>
+   */
+  public MatchUserEventStat getMatchUserEventStat(Long matchId,Long userId) {
+    int goals=0;
+    int assists=0;
+    int fouls=0;
+
+    //쿼리 결과 받아서 변수에 대입
+    List<EventTypeCount> eventTypeCounts= matchEventRepository.countEventTypesByUserAndMatch(matchId,userId);
+    for (EventTypeCount eventTypeCount : eventTypeCounts) {
+      String eventType = eventTypeCount.getEventType();
+      Long count = eventTypeCount.getCount();
+
+      if ("GOALS".equals(eventType)) {
+        goals = count.intValue();
+      } else if ("ASSISTS".equals(eventType)) {
+        assists = count.intValue();
+      } else if ("FOULS".equals(eventType)) {
+        fouls = count.intValue();
+      }
+    }
+
+    //쿼리로 얻은 변수로 DTO 만들어 결과값으로 리턴
+    return MatchUserEventStat.builder().
+        goals(goals).
+        assists(assists).
+        fouls(fouls).
+        build();
+  }
+}
