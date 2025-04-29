315189c341376930ebda56226cd253b6240a60da
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-29 15:16:01 +0900

    feat(matchEvent): 특정 Match에서 특정 User가 발생시킨 Event 조회하는 쿼리 생성

--- src/main/java/com/matchday/matchdayserver/matchevent/repository/MatchEventRepository.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/repository/MatchEventRepository.java
@@ -1,7 +1,21 @@
 package com.matchday.matchdayserver.matchevent.repository;
 
+import com.matchday.matchdayserver.matchevent.model.dto.EventTypeCount;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matchday.matchdayserver.matchevent.model.entity.MatchEvent;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
-public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> { }
+public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> {
+  @Query(value = """
+    SELECT 
+      event_type AS eventType,
+      COUNT(*) AS count
+    FROM match_event
+    WHERE match_id = :matchId AND user_id = :userId
+    GROUP BY event_type
+    """, nativeQuery = true)
+  List<EventTypeCount> countEventTypesByUserAndMatch(@Param("matchId") Long matchId, @Param("userId") Long userId);
+}
