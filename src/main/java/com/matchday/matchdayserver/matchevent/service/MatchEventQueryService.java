8aa57e611c1bd0def28dfcca860185c9c2b4c607
JoonHyuk0331 <joonlife0901@gmail.com>
2025-04-30 20:54:46 +0900

    fix(MatchEvent): final 필드에 의한 의존성 주입 실패 문제 해결

--- src/main/java/com/matchday/matchdayserver/matchevent/service/MatchEventQueryService.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/service/MatchEventQueryService.java
@@ -11,7 +11,7 @@ import java.util.List;
 @RequiredArgsConstructor
 public class MatchEventQueryService {
 
-  private MatchEventRepository matchEventRepository;
+  private final MatchEventRepository matchEventRepository;
   /**
    * MatchEventStrategy
    * <p>
