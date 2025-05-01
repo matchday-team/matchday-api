905aaa5f1a6eab661148f25bbc116484b26af1ef
roky <rookedsysc36@gmail.com>
2025-05-02 02:14:51 +0900

    feat: MatchEvent 이력 조회 API 구현

--- src/main/java/com/matchday/matchdayserver/matchevent/repository/MatchEventRepository.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/repository/MatchEventRepository.java
@@ -3,5 +3,17 @@ package com.matchday.matchdayserver.matchevent.repository;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matchday.matchdayserver.matchevent.model.entity.MatchEvent;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import java.util.List;
 
-public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> { }
+public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> {
+
+  @Query("""
+          select me from MatchEvent me
+          join fetch me.matchUser as mu
+          join fetch me.match as m
+          where m.id = :id
+      """)
+  List<MatchEvent> findByMatchIdFetchMatchUserAndMatch(@Param("id") Long matchId);
+}
