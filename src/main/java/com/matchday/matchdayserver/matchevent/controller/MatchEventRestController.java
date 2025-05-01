0daf10e446faaf2da97a1bb50a8664ce9b493702
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-01 15:43:24 +0900

    feat(MatchEvent): RestApi 전용 MatchEventRestController 구현

---
+++ src/main/java/com/matchday/matchdayserver/matchevent/controller/MatchEventRestController.java
@@ -0,0 +1,26 @@
+package com.matchday.matchdayserver.matchevent.controller;
+
+import com.matchday.matchdayserver.common.response.ApiResponse;
+import com.matchday.matchdayserver.matchevent.service.MatchEventDeleteService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@Tag(name = "match-event", description = "매치 이벤트 관련 API")
+@RequestMapping("/api/v1/match-event")
+@RestController
+@RequiredArgsConstructor
+public class MatchEventRestController {
+
+  private final MatchEventDeleteService matchEventDeleteService;
+
+  @Operation(summary = "이벤트 전체 삭제" , description = "matchId 해당하는 matchEvent 전체삭제")
+  @DeleteMapping("/{matchId}")
+  public ApiResponse<Long> deleteAllEvents(@PathVariable Long matchId) {
+    return ApiResponse.ok(matchEventDeleteService.deleteAllEvents(matchId));
+  }
+}
