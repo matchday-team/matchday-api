ddfc3cb2054d4e6840e799bb7c25fa26c5de0704
Juri-Jang <jjr84@dgu.ac.kr>
2025-05-01 16:14:18 +0900

    feat(Team): 특정 유저가 팀 활동 중이 아닌 경우 상태 추가

--- src/main/java/com/matchday/matchdayserver/common/response/TeamStatus.java
+++ src/main/java/com/matchday/matchdayserver/common/response/TeamStatus.java
@@ -3,7 +3,9 @@ package com.matchday.matchdayserver.common.response;
 public enum TeamStatus implements StatusInterface {
     DUPLICATE_TEAMNAME(400, 5001, "이미 존재하는 팀 이름"),
     NOTFOUND_TEAM(404,5002, "존재하지 않는 팀입니다"),
-    ALREADY_JOINED_USER(400,5003, "팀에 이미 유저가 소속되어있음");
+    ALREADY_JOINED_USER(400,5003, "팀에 이미 유저가 소속되어있음"),
+    INACTIVE_USER_TEAM(400, 5004, "현재 해당 팀에 활동 중이 아닌 사용자입니다")
+  ;
 
     private final int httpStatusCode;
     private final int customStatusCode;
