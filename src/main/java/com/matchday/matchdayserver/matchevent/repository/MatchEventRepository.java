49d4affabf7050c115f3218e5ac1cf5ed5db8624
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-01 15:44:29 +0900

    feat(MatchEvent): DB 삭제 로직 구현

--- src/main/java/com/matchday/matchdayserver/matchevent/repository/MatchEventRepository.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/repository/MatchEventRepository.java
@@ -4,4 +4,6 @@ import org.springframework.data.jpa.repository.JpaRepository;
 
 import com.matchday.matchdayserver.matchevent.model.entity.MatchEvent;
 
-public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> { }
+public interface MatchEventRepository extends JpaRepository<MatchEvent, Long> {
+  void deleteByMatchId(Long matchId);
+}
