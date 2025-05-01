dac7f62d994e5b33e0503aff9ec957d1df82c413
roky <rookedsysc36@gmail.com>
2025-05-01 23:04:14 +0900

    feat(MatchUserMapper): team 추가

--- src/main/java/com/matchday/matchdayserver/matchuser/model/mapper/MatchUserMapper.java
+++ src/main/java/com/matchday/matchdayserver/matchuser/model/mapper/MatchUserMapper.java
@@ -3,14 +3,16 @@ package com.matchday.matchdayserver.matchuser.model.mapper;
 import com.matchday.matchdayserver.match.model.entity.Match;
 import com.matchday.matchdayserver.matchuser.model.dto.MatchUserCreateRequest;
 import com.matchday.matchdayserver.matchuser.model.entity.MatchUser;
+import com.matchday.matchdayserver.team.model.entity.Team;
 import com.matchday.matchdayserver.user.model.entity.User;
 
 public class MatchUserMapper {
     //MatchUser Dto -> Entity 변환
-    public static MatchUser toMatchUser(Match match, User user, MatchUserCreateRequest request) {
+    public static MatchUser toMatchUser(Match match, User user, Team team, MatchUserCreateRequest request) {
         return MatchUser.builder()
                 .match(match)
                 .user(user)
+                .team(team)
                 .role(request.getRole())
                 .matchPosition(request.getMatchPosition())
                 .matchGrid(request.getMatchGrid())
