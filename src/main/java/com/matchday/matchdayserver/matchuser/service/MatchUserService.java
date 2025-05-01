f95d8f7d644940e326d7312d2d8708332b7aa86d
Juri-Jang <jjr84@dgu.ac.kr>
2025-05-01 16:16:26 +0900

    feat(MatchUser): MachUser 등록시 유저가 특정 팀 활동 중이 아닌 경우 예외 처리 추가

--- src/main/java/com/matchday/matchdayserver/matchuser/service/MatchUserService.java
+++ src/main/java/com/matchday/matchdayserver/matchuser/service/MatchUserService.java
@@ -2,6 +2,7 @@ package com.matchday.matchdayserver.matchuser.service;
 
 import com.matchday.matchdayserver.common.exception.ApiException;
 import com.matchday.matchdayserver.common.response.MatchUserStatus;
+import com.matchday.matchdayserver.common.response.TeamStatus;
 import com.matchday.matchdayserver.common.response.UserStatus;
 import com.matchday.matchdayserver.match.model.entity.Match;
 import com.matchday.matchdayserver.match.repository.MatchRepository;
@@ -11,6 +12,8 @@ import com.matchday.matchdayserver.matchuser.model.mapper.MatchUserMapper;
 import com.matchday.matchdayserver.matchuser.repository.MatchUserRepository;
 import com.matchday.matchdayserver.user.model.entity.User;
 import com.matchday.matchdayserver.user.repository.UserRepository;
+import com.matchday.matchdayserver.userteam.model.entity.UserTeam;
+import com.matchday.matchdayserver.userteam.repository.UserTeamRepository;
 import jakarta.transaction.Transactional;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -21,6 +24,7 @@ public class MatchUserService {
     private final MatchUserRepository matchUserRepository;
     private final MatchRepository matchRepository;
     private final UserRepository userRepository;
+    private final UserTeamRepository userTeamRepository;
 
     @Transactional
     public void create(Long matchId, MatchUserCreateRequest request){
@@ -28,6 +32,12 @@ public class MatchUserService {
                 .orElseThrow(() -> new ApiException(MatchUserStatus.NOTFOUND_MATCH));
         User user = userRepository.findById(request.getUserId())
                 .orElseThrow(() -> new ApiException(UserStatus.NOTFOUND_USER));
+
+
+        //현재 해당 팀에서 활동 중인지 여부 확인
+        UserTeam userTeam = userTeamRepository.findActiveUserInHomeTeam(user.getId(), match.getId())
+            .orElseThrow(() -> new ApiException(TeamStatus.INACTIVE_USER_TEAM));
+
 
         //중복 등록 여부 확인
         if (matchUserRepository.existsByMatchIdAndUserId(matchId, request.getUserId())) {
--- src/main/java/com/matchday/matchdayserver/userteam/repository/UserTeamRepository.java
+++ src/main/java/com/matchday/matchdayserver/userteam/repository/UserTeamRepository.java
@@ -2,10 +2,20 @@ package com.matchday.matchdayserver.userteam.repository;
 
 import com.matchday.matchdayserver.userteam.model.entity.UserTeam;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
+import java.util.Optional;
 
 public interface UserTeamRepository extends JpaRepository<UserTeam, Long> {
     boolean existsByUserIdAndTeamId(Long userId, Long teamId);
     List<UserTeam> findAllByTeamId(Long teamId);
+
+    @Query("""
+    SELECT ut FROM UserTeam ut
+    JOIN Match m ON m.homeTeam.id = ut.team.id
+    WHERE ut.user.id = :userId AND m.id = :matchId AND ut.isActive = true
+    """)
+    Optional<UserTeam> findActiveUserInHomeTeam(Long userId, Long matchId);
+
 }
