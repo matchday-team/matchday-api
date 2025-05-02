7b4b60fb70bca5ef1ce1a30fc7037b8fb0aba965
roky <rookedsysc36@gmail.com>
2025-05-02 17:11:30 +0900

    feat: 선수 교체 WebSocket 구현

---
+++ src/main/java/com/matchday/matchdayserver/matchuser/service/MatchUserExchangeService.java
@@ -0,0 +1,106 @@
+package com.matchday.matchdayserver.matchuser.service;
+
+import java.time.LocalDateTime;
+import java.util.ArrayList;
+import java.util.List;
+
+import com.matchday.matchdayserver.common.response.DefaultStatus;
+import com.matchday.matchdayserver.common.response.UserStatus;
+import com.matchday.matchdayserver.matchevent.mapper.MatchEventMapper;
+import com.matchday.matchdayserver.matchevent.model.dto.MatchEventResponse;
+import com.matchday.matchdayserver.matchevent.model.entity.MatchEvent;
+import com.matchday.matchdayserver.matchevent.model.enums.MatchEventType;
+import com.matchday.matchdayserver.matchevent.repository.MatchEventRepository;
+import org.springframework.messaging.simp.SimpMessagingTemplate;
+import org.springframework.stereotype.Service;
+
+import com.matchday.matchdayserver.common.exception.ApiException;
+import com.matchday.matchdayserver.common.model.Message;
+import com.matchday.matchdayserver.common.response.MatchStatus;
+import com.matchday.matchdayserver.matchuser.model.dto.MatchUserExchangeRequest;
+import com.matchday.matchdayserver.matchuser.model.entity.MatchUser;
+import com.matchday.matchdayserver.matchuser.repository.MatchUserRepository;
+
+import jakarta.transaction.Transactional;
+import lombok.RequiredArgsConstructor;
+
+@Service
+@Transactional
+@RequiredArgsConstructor
+public class MatchUserExchangeService {
+
+    private final MatchUserRepository matchUserRepository;
+    private final SimpMessagingTemplate messagingTemplate;
+    private final MatchEventRepository matchEventRepository;
+
+    public void exchangeMatchUser(Long matchId, Message<MatchUserExchangeRequest> request) {
+        validateRequest(request);
+        validateAuthUser(matchId, request.getToken());
+
+        MatchUser fromMatchUser = matchUserRepository.findById(
+                request.getData().getFromMatchUserId())
+            .orElseThrow(() -> new ApiException(MatchStatus.NOT_PARTICIPATING_PLAYER));
+
+        MatchUser toMatchUser = matchUserRepository
+            .findByMatchIdAndUserIdWithFetch(matchId, request.getData().getToUserId())
+            .orElseThrow(() -> new ApiException(MatchStatus.NOT_PARTICIPATING_PLAYER));
+
+        // Dirty Checking으로 자동 저장
+        fromMatchUser.exchange(toMatchUser);
+
+        List<MatchEvent> matchEvents = List.of(
+            MatchEvent.builder()
+                .eventTime(LocalDateTime.now())
+                .eventType(MatchEventType.SUB_IN)
+                .description(request.getData().getMessage())
+                .match(toMatchUser.getMatch())
+                .matchUser(toMatchUser)
+                .build(),
+            MatchEvent.builder()
+                .eventTime(LocalDateTime.now())
+                .eventType(MatchEventType.SUB_OUT)
+                .description(request.getData().getMessage())
+                .match(toMatchUser.getMatch())
+                .matchUser(fromMatchUser)
+                .build());
+        matchEventRepository.saveAll(matchEvents);
+
+        List<MatchEventResponse> matchEventResponses = matchEvents.stream()
+            .map(MatchEventMapper::toResponse)
+            .toList();
+
+        for (MatchEventResponse response : matchEventResponses) {
+            messagingTemplate.convertAndSend("/topic/match/" + matchId, response);
+        }
+    }
+
+    private void validateAuthUser(Long matchId, String token) {
+        Long authId = Long.parseLong(token);
+
+        MatchUser authUser = matchUserRepository
+            .findByMatchIdAndUserIdWithFetch(matchId, authId)
+            .orElseThrow(() -> new ApiException(UserStatus.NOTFOUND_USER));
+
+        if (!authUser.getMatch().getId().equals(matchId)) {
+            throw new ApiException(MatchStatus.NOT_PARTICIPATING_PLAYER);
+        }
+    }
+
+    private void validateRequest(Message<MatchUserExchangeRequest> request) {
+        List<String> errorMessages = new ArrayList<>();
+        if (request.getToken() == null) {
+            errorMessages.add("token은 필수 입력 값입니다.");
+        }
+
+        if (request.getData().getFromMatchUserId() == null
+            || request.getData().getToUserId() == null) {
+            errorMessages.add("fromMatchUserId, toUserId는 필수 입력 값입니다.");
+        }
+
+        if (!errorMessages.isEmpty()) {
+            DefaultStatus defaultStatus = DefaultStatus.BAD_REQUEST;
+            defaultStatus.setCustomDescription(String.join("\n", errorMessages));
+            throw new ApiException(defaultStatus);
+        }
+    }
+}
