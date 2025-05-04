67e04e2398bc78146cc160375a77900e32b84d4a
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-04 21:19:12 +0900

    feat(matchUser): 선발/교체, 옐로 레드 카드 개수,퇴장 여부,경고 개수 포함한 필드 추가

--- src/main/java/com/matchday/matchdayserver/matchuser/model/dto/MatchUserGroupResponse.java
+++ src/main/java/com/matchday/matchdayserver/matchuser/model/dto/MatchUserGroupResponse.java
@@ -1,16 +1,38 @@
 package com.matchday.matchdayserver.matchuser.model.dto;
 
+import com.matchday.matchdayserver.matchuser.model.dto.MatchUserResponse;
+import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
+import lombok.Getter;
 import lombok.NoArgsConstructor;
 import java.util.List;
 
+@Data
 @Builder
 @AllArgsConstructor
 @NoArgsConstructor
-@Data
+@Schema(description = "홈팀과 어웨이팀 참가자 정보를 선발/교체로 그룹핑한 응답")
 public class MatchUserGroupResponse {
-    private List<MatchUserResponse> homeTeam;
-    private List<MatchUserResponse> awayTeam;
+
+    @Schema(description = "홈팀 참가자 정보 (선발/교체)")
+    private TeamGroupedUsers homeTeam;
+
+    @Schema(description = "어웨이팀 참가자 정보 (선발/교체)")
+    private TeamGroupedUsers awayTeam;
+
+    @Data
+    @Builder
+    @AllArgsConstructor
+    @NoArgsConstructor
+    @Schema(description = "선발 및 교체 참가자 그룹")
+    public static class TeamGroupedUsers {
+
+        @Schema(description = "선발 선수 목록")
+        private List<MatchUserResponse> starters;
+
+        @Schema(description = "교체 선수 목록")
+        private List<MatchUserResponse> substitutes;
+    }
 }
