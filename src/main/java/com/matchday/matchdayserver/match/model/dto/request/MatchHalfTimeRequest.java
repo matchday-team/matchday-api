e5660b1f237a5cf646666911e975863092314431
Juri-Jang <jjr84@dgu.ac.kr>
2025-05-04 14:46:44 +0900

    feat(Match): 전,후반 시간 등록 요청 dto 추가

---
+++ src/main/java/com/matchday/matchdayserver/match/model/dto/request/MatchHalfTimeRequest.java
@@ -0,0 +1,14 @@
+package com.matchday.matchdayserver.match.model.dto.request;
+
+import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.Getter;
+import java.time.LocalTime;
+
+@Getter
+public class MatchHalfTimeRequest {
+    @Schema(description = "시작 시간")
+    private LocalTime startTime;
+
+    @Schema(description = "종료 시간")
+    private LocalTime endTime;
+}
