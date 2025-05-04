6adf1e0c24d38108da136d608d85577806eceed4
Juri-Jang <jjr84@dgu.ac.kr>
2025-05-04 15:05:09 +0900

    refactor(Match): MatchHalfTimeRequest 생성자 추가

--- src/main/java/com/matchday/matchdayserver/match/model/dto/request/MatchHalfTimeRequest.java
+++ src/main/java/com/matchday/matchdayserver/match/model/dto/request/MatchHalfTimeRequest.java
@@ -2,9 +2,11 @@ package com.matchday.matchdayserver.match.model.dto.request;
 
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Getter;
+import lombok.NoArgsConstructor;
 import java.time.LocalTime;
 
 @Getter
+@NoArgsConstructor
 public class MatchHalfTimeRequest {
     @Schema(description = "시작 시간")
     private LocalTime startTime;
