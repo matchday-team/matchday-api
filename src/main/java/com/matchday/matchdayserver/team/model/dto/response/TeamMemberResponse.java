45cab58b878df4a14b8c50faf00919431b84cc5b
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-08 00:05:17 +0900

    feat(team): TeamMemberResponse 프로필 url 필드 추가

--- src/main/java/com/matchday/matchdayserver/team/model/dto/response/TeamMemberResponse.java
+++ src/main/java/com/matchday/matchdayserver/team/model/dto/response/TeamMemberResponse.java
@@ -12,13 +12,15 @@ public class TeamMemberResponse {
     private Integer number;
     private String defaultPosition;
     private Boolean isActive; //현재 팀에서 활동 여부
+    private String imageUrl; //프로필 이미지 url
 
     @Builder
-    public TeamMemberResponse(Long id, String name, Integer number, String defaultPosition, Boolean isActive) {
+    public TeamMemberResponse(Long id, String name, Integer number, String defaultPosition, Boolean isActive,String imageUrl) {
         this.id = id;
         this.name = name;
         this.number = number;
         this.defaultPosition = defaultPosition;
         this.isActive = isActive;
+        this.imageUrl = imageUrl;
     }
 }
