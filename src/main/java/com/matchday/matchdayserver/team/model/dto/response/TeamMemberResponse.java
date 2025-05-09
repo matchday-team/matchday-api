da5c95a65ebaa1234c9e90ed06954efb226981c2
Juri-Jang <jjr84@dgu.ac.kr>
2025-05-10 07:11:27 +0900

    modify: 불필요 import 제거

--- src/main/java/com/matchday/matchdayserver/team/model/dto/response/TeamMemberResponse.java
+++ src/main/java/com/matchday/matchdayserver/team/model/dto/response/TeamMemberResponse.java
@@ -1,6 +1,5 @@
 package com.matchday.matchdayserver.team.model.dto.response;
 
-import com.fasterxml.jackson.annotation.JsonInclude;
 import io.swagger.v3.oas.annotations.media.Schema;
 import jakarta.validation.constraints.NotNull;
 import lombok.*;
