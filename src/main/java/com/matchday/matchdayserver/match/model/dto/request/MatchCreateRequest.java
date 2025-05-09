6a9f816c1a442a12e11b91fb1ac3daf2b7d4b34f
Juri-Jang <jjr84@dgu.ac.kr>
2025-05-09 18:57:56 +0900

    refactor(match): startTime, endTime명 변경 (plannedStartTime/EndTime) 및 전,후반 종료시간 추가

--- src/main/java/com/matchday/matchdayserver/match/model/dto/request/MatchCreateRequest.java
+++ src/main/java/com/matchday/matchdayserver/match/model/dto/request/MatchCreateRequest.java
@@ -29,10 +29,10 @@ public class MatchCreateRequest {
   private LocalDate matchDate;
 
   @Schema(description = "경기 시작 시간", example = "14:00:00")
-  private LocalTime startTime;
+  private LocalTime plannedStartTime;
 
   @Schema(description = "경기 종료 시간", example = "16:00:00")
-  private LocalTime endTime;
+  private LocalTime plannedEndTime;
 
   @Schema(description = "주심", example = "주심 이름", nullable = true)
   private String mainRefereeName;
