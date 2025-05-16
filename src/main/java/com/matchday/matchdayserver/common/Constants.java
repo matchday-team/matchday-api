1a8a4de06994c6de48f8fbfc1a2d9f31c9fc365d
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-16 20:52:53 +0900

    feat(WebConfig): swagger url 명시로 인한 localhost:8080 cors 오류 해소

--- src/main/java/com/matchday/matchdayserver/common/Constants.java
+++ src/main/java/com/matchday/matchdayserver/common/Constants.java
@@ -3,6 +3,7 @@ package com.matchday.matchdayserver.common;
 public class Constants {
 
   public static final String FRONTEND_LOCAL_URL = "http://localhost:5173";
+  public static final String BACKEND_LOCAL_URL = "http://localhost:8080"; //swagger
   public static final String FRONTEND_PRODUCTION_URL = "https://match-day.netlify.app";
   public static final String BACKEND_PRODUCTION_URL = "https://dev-api.matchday-planner.com";
 
