ad218c1a374107eaa6f04ee0205c5d98839ae76b
Juri-Jang <jjr84@dgu.ac.kr>
2025-05-17 17:07:46 +0900

    modify(matchUser): 선수 교체시 교체 선수의 grid만 복사되게 수정

--- src/main/java/com/matchday/matchdayserver/matchuser/model/entity/MatchUser.java
+++ src/main/java/com/matchday/matchdayserver/matchuser/model/entity/MatchUser.java
@@ -62,18 +62,14 @@ public class MatchUser {
         this.matchGrid = matchGrid;
     }
 
-    public void exchange(MatchUser toMatchUser) {
-        if (!toMatchUser.getTeam().getId().equals(this.team.getId())) {
+    public void substituteTo(MatchUser subInPlayer) {
+        if (!subInPlayer.getTeam().getId().equals(this.team.getId())) {
             throw new ApiException(MatchStatus.DIFFERENT_TEAM_EXCHANGE);
         }
-        String originalPosition = toMatchUser.getMatchPosition();
-        int originalGrid = toMatchUser.getMatchGrid();
-        toMatchUser.updateMatchPosition(this.matchPosition);
-        toMatchUser.updateMatchGrid(this.matchGrid);
 
-        this.matchPosition = originalPosition;
-        this.matchGrid = originalGrid;
+        subInPlayer.updateMatchGrid(this.matchGrid);
     }
+
 
     // 퇴장시키는 메서드
     public void sendOff() {
