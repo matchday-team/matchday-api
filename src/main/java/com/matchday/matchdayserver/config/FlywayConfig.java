baf64057a649e13bb84f624e8e28c63d39003986
roky <rookedsysc36@gmail.com>
2025-05-17 22:36:46 +0900

    feat: repairMigrationStrategy() 작성

---
+++ src/main/java/com/matchday/matchdayserver/config/FlywayConfig.java
@@ -0,0 +1,16 @@
+package com.matchday.matchdayserver.config;
+
+import org.springframework.boot.autoconfigure.flyway.FlywayMigrationStrategy;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+public class FlywayConfig {
+    @Bean
+    public FlywayMigrationStrategy repairMigrationStrategy() {
+        return flyway -> {
+            flyway.repair();
+            flyway.migrate();
+        };
+    }
+}
