d217154a1ec7e9221c68859f9d660e11419a4c6b
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-19 23:17:54 +0900

    feat(memo): 응답값에 matchId 값 추가

--- src/main/java/com/matchday/matchdayserver/match/model/dto/response/MatchMemoResponse.java
+++ src/main/java/com/matchday/matchdayserver/match/model/dto/response/MatchMemoResponse.java
@@ -1,6 +1,7 @@
 package com.matchday.matchdayserver.match.model.dto.response;
 
 import io.swagger.v3.oas.annotations.media.Schema;
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.Setter;
@@ -8,8 +9,12 @@ import lombok.Setter;
 @Getter
 @Setter
 @Builder
+@AllArgsConstructor
 @Schema(description = "매치별 메모 응답 객체")
 public class MatchMemoResponse {
+
+    @Schema(description = "메모id")
+    private Long matchId;
 
     @Schema(description = "메모", nullable = true)
     private String memo;
