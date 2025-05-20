30f376997d14596c3604bacc478e07f92a03e3ce
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-21 00:32:45 +0900

    feat(sql): enum MatchState에 HALF_TIME(쉬는시간) 추가

--- src/main/java/com/matchday/matchdayserver/match/model/enums/MatchState.java
+++ src/main/java/com/matchday/matchdayserver/match/model/enums/MatchState.java
@@ -3,5 +3,6 @@ package com.matchday.matchdayserver.match.model.enums;
 public enum MatchState {
     SCHEDULED,  // 경기 전
     IN_PLAY,    // 진행 중
-    FINISHED
+    FINISHED,
+    HALF_TIME //쉬는시간
 }
