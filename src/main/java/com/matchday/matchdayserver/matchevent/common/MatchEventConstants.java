f7331a8f91e21f0516650f5e2fabb1ac7e5cf764
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-20 15:00:20 +0900

    refactor(MatchEvent): 확장성을 위해 MATCH_EVENT_URL_PREFIX -> EVENT에만 국한되지 않도록 네이밍 변경 MATCH_URL_PREFIX , getMemoTopic 메소드 추가

--- src/main/java/com/matchday/matchdayserver/matchevent/common/MatchEventConstants.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/common/MatchEventConstants.java
@@ -1,9 +1,11 @@
 package com.matchday.matchdayserver.matchevent.common;
 
 public class MatchEventConstants {
-    public static final String MATCH_EVENT_URL_PREFIX = "/topic/match/";
+    public static final String MATCH_URL_PREFIX  = "/topic/match/";
 
     public static String getMatchEventUrl(Long matchId) {
-        return MATCH_EVENT_URL_PREFIX + matchId;
+        return MATCH_URL_PREFIX  + matchId;
     }
+
+    public static String getMemoTopic(Long matchId) { return MATCH_URL_PREFIX  + matchId + "/memo"; }
 }
