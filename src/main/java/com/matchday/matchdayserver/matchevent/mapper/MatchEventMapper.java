b55159434a28be81ac4696e23c45ed9ef6c69a10
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-21 02:09:54 +0900

    feat(matchevent): MatchState 기준으로 전/후반 여부를 판단하는 로직 추가

--- src/main/java/com/matchday/matchdayserver/matchevent/mapper/MatchEventMapper.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/mapper/MatchEventMapper.java
@@ -2,6 +2,8 @@ package com.matchday.matchdayserver.matchevent.mapper;
 
 import com.matchday.matchdayserver.common.exception.ApiException;
 import com.matchday.matchdayserver.common.response.MatchStatus;
+import com.matchday.matchdayserver.match.model.enums.HalfType;
+import com.matchday.matchdayserver.match.model.enums.MatchState;
 import com.matchday.matchdayserver.matchevent.model.dto.MatchEventRequest;
 import com.matchday.matchdayserver.matchevent.model.dto.MatchEventResponse;
 import com.matchday.matchdayserver.matchevent.model.entity.MatchEvent;
@@ -13,6 +15,9 @@ import com.matchday.matchdayserver.user.model.entity.User;
 import java.time.Duration;
 import java.time.LocalDateTime;
 import java.util.Optional;
+
+import static com.matchday.matchdayserver.match.model.enums.HalfType.FIRST_HALF;
+import static com.matchday.matchdayserver.match.model.enums.HalfType.SECOND_HALF;
 
 public class MatchEventMapper {
 
@@ -27,18 +32,15 @@ public class MatchEventMapper {
 
     public static MatchEventResponse toResponse(MatchEvent matchEvent) {
         Match match = matchEvent.getMatch();
+        HalfType halfType;
         //만약 second_half_start_time 이 null 값이라면 아직 전반전인 것임
         LocalDateTime matchStartTime;
-        if (match.getSecondHalfStartTime() == null) {
-            if (match.getFirstHalfStartTime() == null || match.getMatchDate() == null) {
-                throw new ApiException(MatchStatus.INVALID_MATCH_TIME);
-            }
+        if (match.getMatchState() == MatchState.PLAY_FIRST_HALF) {
             matchStartTime = match.getFirstHalfStartTime().atDate(match.getMatchDate());//핵심
+            halfType=FIRST_HALF;
         } else {
-            if (match.getMatchDate() == null) {
-                throw new ApiException(MatchStatus.INVALID_MATCH_TIME);
-            }
             matchStartTime = match.getSecondHalfStartTime().atDate(match.getMatchDate());//핵심
+            halfType=SECOND_HALF;
         }
 
         Long elapsedMinutes = calculateElapsedMinutes(
@@ -50,6 +52,7 @@ public class MatchEventMapper {
         return MatchEventResponse.builder()
             .id(matchEvent.getId())
             .elapsedMinutes(elapsedMinutes)
+            .halfType(halfType)
             .teamId(team.getId())
             .teamName(team.getName())
             .userId(user.getId())
