af8c8ab262dee21d25f191d2f3017279763f80ae
roky <rookedsysc36@gmail.com>
2025-05-20 21:31:15 +0900

    modify: 조건 잘못준 부분 수정

--- src/main/java/com/matchday/matchdayserver/match/util/MatchStateValidator.java
+++ src/main/java/com/matchday/matchdayserver/match/util/MatchStateValidator.java
@@ -10,7 +10,7 @@ public class MatchStateValidator {
     private MatchStateValidator() {} // 유틸클래스니까
 
     public static void validateInPlay(Match match) {
-        if (match.getMatchState().equals(MatchState.IN_PLAY)) {
+        if (!match.getMatchState().equals(MatchState.IN_PLAY)) {
             throw new ApiException(MatchStatus.NOT_IN_PLAY_MATCH);
         }
     }
