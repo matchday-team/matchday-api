8ad4a6c824607b93b668c2fa5a255e6f46508ce2
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-21 01:51:44 +0900

    feat(sql): 전후반 구분을 위해 IN_PLAY 를 'PLAY_FIRST_HALF','PLAY_SECOND_HALF' 으로 이원화

--- src/main/java/com/matchday/matchdayserver/match/model/enums/MatchState.java
+++ src/main/java/com/matchday/matchdayserver/match/model/enums/MatchState.java
@@ -1,8 +1,9 @@
 package com.matchday.matchdayserver.match.model.enums;
 
 public enum MatchState {
-    SCHEDULED,  // 경기 전
-    IN_PLAY,    // 진행 중
-    FINISHED,
-    HALF_TIME //쉬는시간
+    SCHEDULED,        // 경기 전
+    PLAY_FIRST_HALF,  // 전반 진행 중
+    HALF_TIME,        // 전반과 후반 사이 쉬는 시간
+    PLAY_SECOND_HALF, // 후반 진행 중
+    FINISHED          // 경기 종료
 }
