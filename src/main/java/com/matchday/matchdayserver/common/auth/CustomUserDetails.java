de37751d796cc5abc36460b317eaf65f7a4f16c7
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-22 16:12:23 +0900

    feat(auth): UserId를 authentication에 넣을 수 있게 커스텀 userDetail 클래스 정의

--- src/main/java/com/matchday/matchdayserver/common/auth/CustomUserDetails.java
+++ src/main/java/com/matchday/matchdayserver/common/auth/CustomUserDetails.java
@@ -1,5 +1,58 @@
 package com.matchday.matchdayserver.common.auth;
 
-public class CustomUserDetails {
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import java.util.Collection;
+import java.util.List;
 
+public class CustomUserDetails implements UserDetails {
+
+    private final Long userId;
+    private final String username;
+    private final List<GrantedAuthority> authorities;
+
+    public CustomUserDetails(Long userId, String username, List<GrantedAuthority> authorities) {
+        this.userId = userId;
+        this.username = username;
+        this.authorities = authorities;
+    }
+
+    public Long getUserId() {
+        return userId;
+    }
+
+    @Override
+    public Collection<? extends GrantedAuthority> getAuthorities() {
+        return List.of();
+    }
+
+    @Override
+    public String getPassword() {
+        return "";
+    }
+
+    @Override
+    public String getUsername() {
+        return "";
+    }
+
+    @Override
+    public boolean isAccountNonExpired() {
+        return UserDetails.super.isAccountNonExpired();
+    }
+
+    @Override
+    public boolean isAccountNonLocked() {
+        return UserDetails.super.isAccountNonLocked();
+    }
+
+    @Override
+    public boolean isCredentialsNonExpired() {
+        return UserDetails.super.isCredentialsNonExpired();
+    }
+
+    @Override
+    public boolean isEnabled() {
+        return UserDetails.super.isEnabled();
+    }
 }
