63fe304aabe03d99c21a27b96f2f53f84a4c0646
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-25 18:16:41 +0900

    chore(Constants): OAuth 테스트용 FRONTEND_LOCAL_VUE_URL 상수 추가

--- src/main/java/com/matchday/matchdayserver/common/Constants.java
+++ src/main/java/com/matchday/matchdayserver/common/Constants.java
@@ -6,6 +6,6 @@ public class Constants {
   public static final String BACKEND_LOCAL_URL = "http://localhost:8080"; //swagger
   public static final String FRONTEND_PRODUCTION_URL = "https://match-day.netlify.app";
   public static final String BACKEND_PRODUCTION_URL = "https://dev-api.matchday-planner.com";
-
+  public static final String FRONTEND_LOCAL_VUE_URL = "http://localhost:3000";
 
 }
