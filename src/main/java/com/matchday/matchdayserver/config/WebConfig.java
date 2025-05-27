a5a537fcb7926ab50fcd90b90bd907fafeeb62cb
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-27 17:41:51 +0900

    feat(WebConfig): @userSession 파라미터 자동 주입 위한 ArgumentResolver 등록

--- src/main/java/com/matchday/matchdayserver/config/WebConfig.java
+++ src/main/java/com/matchday/matchdayserver/config/WebConfig.java
@@ -1,6 +1,7 @@
 package com.matchday.matchdayserver.config;
 
 import com.matchday.matchdayserver.common.resolver.UserIdResolver;
+import com.matchday.matchdayserver.common.resolver.UserSessionResolver;
 import lombok.RequiredArgsConstructor;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
@@ -13,18 +14,20 @@ import java.util.List;
 @RequiredArgsConstructor
 public class WebConfig implements WebMvcConfigurer {
 
-private final UserIdResolver userIdResolver;
+    private final UserIdResolver userIdResolver;
+    private final UserSessionResolver userSessionResolver;
 
-  @Override
-  public void addCorsMappings(CorsRegistry registry) {
+    @Override
+    public void addCorsMappings(CorsRegistry registry) {
     registry.addMapping("/**")
         .allowedOrigins(Constants.FRONTEND_LOCAL_URL, Constants.FRONTEND_PRODUCTION_URL,Constants.BACKEND_PRODUCTION_URL,Constants.BACKEND_LOCAL_URL)
         .allowedMethods("*")
         .allowedHeaders("*");
-  }
+    }
 
     @Override
     public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
         resolvers.add(userIdResolver);
+        resolvers.add(userSessionResolver);
     }
 }
