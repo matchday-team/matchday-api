40cd22f2f26aed018fedd5a28fb363498f7ca224
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-27 18:26:37 +0900

    refactor(SecurityConfig): Swagger 및 Open API 인증 예외 경로 상수화 및 권한 설정 개선

--- src/main/java/com/matchday/matchdayserver/config/SecurityConfig.java
+++ src/main/java/com/matchday/matchdayserver/config/SecurityConfig.java
@@ -2,6 +2,7 @@ package com.matchday.matchdayserver.config;
 
 import com.matchday.matchdayserver.common.auth.JwtTokenFilter;
 import lombok.RequiredArgsConstructor;
+import org.springframework.boot.autoconfigure.security.servlet.PathRequest;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
@@ -10,6 +11,7 @@ import org.springframework.security.config.annotation.web.configurers.AbstractHt
 import org.springframework.security.config.http.SessionCreationPolicy;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
+import java.util.List;
 
 @Configuration
 @EnableWebSecurity(debug = true)//dev환경에서만 사용
@@ -18,24 +20,36 @@ public class SecurityConfig {
 
     private final CorsConfig corsConfig;
     private final JwtTokenFilter jwtTokenFilter;
+    private final List<String> EXCEPTION = List.of("/open-api/**");
+    private final List<String> SWAGGER = List.of("/swagger-ui.html", "/swagger-ui/**", "/swagger-resources/**", "/v2/api-docs", "/v3/api-docs/**");
 
     @Bean
     public SecurityFilterChain security(HttpSecurity httpSecurity) throws Exception {
-        return httpSecurity
+        httpSecurity
             .cors(cors -> cors.configurationSource(corsConfig.myConfigurationSource()))
             .csrf(AbstractHttpConfigurer::disable) //csrf disable
             .httpBasic(AbstractHttpConfigurer::disable)//Basic인증 disable
             .sessionManagement(s -> s.sessionCreationPolicy(SessionCreationPolicy.STATELESS))//세션 disable
             //특정 url패턴에 대해서는 인증처리 제외하고 나머지 url (anyRequest()) 에 대해선 인증처리
-            .authorizeHttpRequests(a -> a.requestMatchers(
-                "/v3/api-docs/**",
-                "/swagger-ui/**",
-                "/swagger-ui.html",
-                "/swagger-resources/**",
-                "/webjars/**",
-                "/api/v1/auth/oauth/google"
-            ).permitAll().anyRequest().authenticated())
-            .addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class)
-            .build();
+            .authorizeHttpRequests(req -> {
+                    req
+                        //필요 : X
+                        .requestMatchers(PathRequest.toStaticResources().atCommonLocations())
+                        .permitAll()
+                        .requestMatchers(SWAGGER.toArray(new String[0]))
+                        .permitAll()
+                        .requestMatchers(EXCEPTION.toArray(new String[0]))
+                        //필요 : 인증,권한
+                        .permitAll()
+                        .requestMatchers("/api/**")
+                        .hasAnyRole("USER")
+                        //필요 : 권한
+                        .anyRequest()
+                        .authenticated();
+                }
+            );
+
+        httpSecurity.addFilterBefore(jwtTokenFilter, UsernamePasswordAuthenticationFilter.class);
+        return httpSecurity.build();
     }
 }
