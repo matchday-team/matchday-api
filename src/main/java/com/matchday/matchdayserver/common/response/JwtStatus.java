882a37074641fc94f88000ac5687237763dd0aaf
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-27 18:23:50 +0900

    refactor(AuthStatus): JWT 예외 상태 enum을 JwtStatus로 리네이밍 및 메시지 개선

--- src/main/java/com/matchday/matchdayserver/common/response/JwtStatus.java
+++ src/main/java/com/matchday/matchdayserver/common/response/JwtStatus.java
@@ -1,10 +1,10 @@
 package com.matchday.matchdayserver.common.response;
 
-public enum AuthStatus implements StatusInterface {
+public enum JwtStatus implements StatusInterface {
     UNAUTHORIZED(401, 9000, "JWT 토큰 검증 실패"),
     INVALID_AUTHORIZATION_HEADER(401, 9001, "올바른 Bearer 형식이 아님, Bearer(공백) 총 7자"),
     EXPIRED_TOKEN(401, 9002, "JWT 토큰이 만료됨"),
-    INVALID_SIGNATURE(401, 9003, "JWT 서명이 위조됨"),
+    SIGNATURE_NOT_MATCH(401, 9003, "JWT 서명이 적절하지 않음"),
     MALFORMED_TOKEN(401, 9004, "JWT 형식이 잘못됨"),
     INVALID_TOKEN(401, 9005, "유효하지 않은 JWT 토큰")
     ;
@@ -13,7 +13,7 @@ public enum AuthStatus implements StatusInterface {
     private final int customStatusCode;
     private String description;
 
-    AuthStatus(int httpStatusCode, int customStatusCode, String description) {
+    JwtStatus(int httpStatusCode, int customStatusCode, String description) {
         this.httpStatusCode = httpStatusCode;
         this.customStatusCode = customStatusCode;
         this.description = description;
