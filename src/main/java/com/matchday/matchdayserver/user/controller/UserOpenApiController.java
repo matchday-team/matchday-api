8442ca71f7ed3881b9d48f22263f6aa0d171b613
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-30 16:13:02 +0900

    feat(auth): 로그인시 쿠키에 리프레시 토큰 추가

--- src/main/java/com/matchday/matchdayserver/user/controller/UserOpenApiController.java
+++ src/main/java/com/matchday/matchdayserver/user/controller/UserOpenApiController.java
@@ -5,7 +5,10 @@ import com.matchday.matchdayserver.auth.model.dto.response.OauthLoginResponse;
 import com.matchday.matchdayserver.auth.service.GoogleOauthService;
 import com.matchday.matchdayserver.common.response.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -15,13 +18,37 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/open-api/v1/users")
 @RestController
 @RequiredArgsConstructor
+@Slf4j
 public class UserOpenApiController {
 
     private final GoogleOauthService googleOauthService;
 
+
     @PostMapping("/google")
-    public ApiResponse<OauthLoginResponse> googleLogin(@RequestBody OauthLoginRequest oAuthLoginRequest){
-        return ApiResponse.ok(googleOauthService.googleLogin(oAuthLoginRequest));
+    public ApiResponse<OauthLoginResponse> googleLogin(@RequestBody OauthLoginRequest oAuthLoginRequest,HttpServletResponse response){
+        OauthLoginResponse loginResponse = googleOauthService.googleLogin(oAuthLoginRequest);
+        response.addCookie(loginResponse.getRefreshTokenCookie());
+
+        return ApiResponse.ok(new OauthLoginResponse(
+                loginResponse.getAccessToken(),
+                null, // refreshToken은 쿠키로 보냈으니 null 처리
+                loginResponse.getId()
+        ));
     }
+
+    @PostMapping("/renew")
+    public ApiResponse<?> renewAccessToken(HttpServletRequest request, HttpServletResponse response){
+
+        //리프레시 토큰 가져오기
+
+        //리프레시 토큰 데이터베이스에 있는지 검증
+
+        //리프레시 토큰이 만료되었는지 확인
+
+        //JWT 만들어 리턴해주기
+            return null;
+    }
+
+
 
 }
