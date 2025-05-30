69d1d278c294b73db9f02965b8ff81d7062ee7cd
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-31 01:09:37 +0900

    refactor(auth): 로그인 로직 리팩토링 및 도메인 책임 분리

--- src/main/java/com/matchday/matchdayserver/user/controller/UserOpenApiController.java
+++ src/main/java/com/matchday/matchdayserver/user/controller/UserOpenApiController.java
@@ -5,13 +5,13 @@ import com.matchday.matchdayserver.auth.model.dto.response.OauthLoginResponse;
 import com.matchday.matchdayserver.auth.model.dto.response.RenewResponse;
 import com.matchday.matchdayserver.auth.service.GoogleOauthService;
 import com.matchday.matchdayserver.common.response.ApiResponse;
+import com.matchday.matchdayserver.user.model.dto.response.LoginResponse;
+import com.matchday.matchdayserver.user.model.entity.User;
 import com.matchday.matchdayserver.user.service.UserOpenApiService;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.servlet.http.Cookie;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 import lombok.RequiredArgsConstructor;
-import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -26,15 +26,17 @@ public class UserOpenApiController {
     private final GoogleOauthService googleOauthService;
     private final UserOpenApiService userOpenApiService;
 
-
     @PostMapping("/google")
     public ApiResponse<OauthLoginResponse> googleLogin(@RequestBody OauthLoginRequest oAuthLoginRequest,HttpServletResponse response){
-        OauthLoginResponse loginResponse = googleOauthService.googleLogin(oAuthLoginRequest);
+        //유저 정보 가져오기
+        User user = googleOauthService.googleLogin(oAuthLoginRequest);
+        //유저 정보로 엑세스토큰과 리프레시 토큰 받아오기
+        LoginResponse loginResponse = userOpenApiService.doLogin(user);
+        //쿠키 세팅
         response.addCookie(loginResponse.getRefreshTokenCookie());
-
+        //엑세스 토큰 리턴
         return ApiResponse.ok(new OauthLoginResponse(
                 loginResponse.getAccessToken(),
-                null, // refreshToken은 쿠키로 보냈으니 null 처리
                 loginResponse.getId()
         ));
     }
