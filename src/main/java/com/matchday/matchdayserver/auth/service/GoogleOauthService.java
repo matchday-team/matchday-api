cf4a29bce8f57b719f0f901c88ff4f3d0aa0ed74
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-31 01:10:10 +0900

    refactor(auth): GoogleOAuthService에서 JWT 발급 로직 제거 및 책임 분리

--- src/main/java/com/matchday/matchdayserver/auth/service/GoogleOauthService.java
+++ src/main/java/com/matchday/matchdayserver/auth/service/GoogleOauthService.java
@@ -26,56 +26,33 @@ import java.util.Map;
 @Slf4j
 public class GoogleOauthService {
 
-    public static final String REFRESH_TOKEN_COOKIE_NAME = "refreshToken";
-
     @Value("${oauth.google.client-id}")
     private String googleClientId;
-
     @Value("${oauth.google.client-secret}")
     private String googleClientSecret;
-
     @Value("${oauth.google.redirect-url}")
     private String googleRedirectUri;
 
     private final UserRepository userRepository;
-    private final JwtTokenProvider jwtTokenProvider;
 
     /**
-     * Google OAuth 로그인 처리
+     * REST API 방식 Google OAuth 로그인 처리
      * 1. 인가 코드로 Google Access Token 요청
      * 2. Google Access Token으로 사용자 정보 조회
      * 3. 기존 유저 조회 or 신규 유저 생성
-     * 4. JWT 토큰 발급 및 반환 <- 이것이 스프링 서버 Access Token
+     * 4. User 객체 반환
      */
-    public OauthLoginResponse googleLogin(OauthLoginRequest oAuthLoginRequest) {
+    public User googleLogin(OauthLoginRequest oAuthLoginRequest) {
+        // 인가 코드로 Google Access Token 요청
         GoogleAccessTokenResponse googleAccessToken = getAccessToken(oAuthLoginRequest.getCode());
+        // Google Access Token으로 사용자 정보 조회
         GoogleProfileResponse googleProfile = getProfile(googleAccessToken.getAccess_token());
-        //유저 가져오기
+        // 기존 유저 조회 or 신규 유저 생성
         User user = userRepository.findBySocialId(googleProfile.getSub());
-        //없으면 만들기
         if (user == null) {
             user = createOatuh(googleProfile, SocialType.GOOGLE);
         }
-        //토큰발급
-        Map<String, Object> payload = new HashMap<>();
-        payload.put("userId", user.getId());
-        payload.put("role", user.getRole());
-
-        String jwtAccessToken=jwtTokenProvider.createToken(user.getEmail(), payload, JwtTokenType.ACCESS);
-        String jwtRefreshToken=jwtTokenProvider.createToken(user.getEmail(), payload, JwtTokenType.REFRESH);
-
-        return new OauthLoginResponse(jwtAccessToken, createCookie(REFRESH_TOKEN_COOKIE_NAME,jwtRefreshToken), user.getId());
-    }
-
-    private Cookie createCookie(String key, String value) {
-
-        Cookie cookie = new Cookie(key, value);
-        cookie.setMaxAge(24*60*60);
-        cookie.setSecure(false); //https (운영환경에선)false
-        cookie.setPath("/"); //전체 경로에 대해 쿠키 유효
-        cookie.setHttpOnly(true);
-
-        return cookie;
+        return user;
     }
 
     /**
@@ -102,7 +79,7 @@ public class GoogleOauthService {
     }
 
     /**
-     * Access Token을 사용하여 Google 사용자 프로필 조회
+     * 구글 Access Token을 사용하여 Google 사용자 프로필 조회
      */
     private GoogleProfileResponse getProfile(String accessToken) {
         RestClient restClient = RestClient.create();
