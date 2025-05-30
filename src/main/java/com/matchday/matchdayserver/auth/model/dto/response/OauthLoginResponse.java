23a64a5d883b5d50b3b710dd49de588a2a2765f2
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-31 01:08:01 +0900

    refactor(OauthLoginResponse): RefreshToken 쿠키 필드를 제거하여 불필요한 응답 제거

--- src/main/java/com/matchday/matchdayserver/auth/model/dto/response/OauthLoginResponse.java
+++ src/main/java/com/matchday/matchdayserver/auth/model/dto/response/OauthLoginResponse.java
@@ -1,6 +1,5 @@
 package com.matchday.matchdayserver.auth.model.dto.response;
 
-import jakarta.servlet.http.Cookie;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
@@ -10,6 +9,5 @@ import lombok.NoArgsConstructor;
 @AllArgsConstructor
 public class OauthLoginResponse {
     String accessToken; //서버 access token
-    Cookie refreshTokenCookie; //서버 refresh token 쿠키 변환값
     Long id;
 }
