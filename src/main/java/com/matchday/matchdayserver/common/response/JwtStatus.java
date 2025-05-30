b07d0bcf89076b265be9048ddd5236c78c34babf
JoonHyuk0331 <joonlife0901@gmail.com>
2025-05-30 23:32:33 +0900

    feat(JwtStatus): JWT 관련 예외 코드 세분화 및 상세 메시지 추가

--- src/main/java/com/matchday/matchdayserver/common/response/JwtStatus.java
+++ src/main/java/com/matchday/matchdayserver/common/response/JwtStatus.java
@@ -3,11 +3,14 @@ package com.matchday.matchdayserver.common.response;
 public enum JwtStatus implements StatusInterface {
     UNAUTHORIZED(401, 9000, "JWT 토큰 검증 실패"),
     INVALID_AUTHORIZATION_HEADER(401, 9001, "올바른 Bearer 형식이 아님, Bearer(공백) 총 7자"),
-    EXPIRED_TOKEN(401, 9002, "JWT 토큰이 만료됨"),
-    SIGNATURE_NOT_MATCH(401, 9003, "JWT 서명이 적절하지 않음"),
-    MALFORMED_TOKEN(401, 9004, "JWT 형식이 잘못됨"),
-    INVALID_TOKEN(401, 9005, "유효하지 않은 JWT 토큰"),
-    INVALID_TOKEN_TYPE(400,9006,"적절한 토큰 타입이 아닙니다")
+    EXPIRED_ACCESS_TOKEN(401, 9002, "JWT 엑세스 토큰이 만료됨"),
+    EXPIRED_REFRESH_TOKEN(401, 9003, "JWT 리프레시 토큰이 만료됨"),
+    SIGNATURE_NOT_MATCH(401, 9004, "JWT 서명이 적절하지 않음"),
+    MALFORMED_TOKEN(401, 9005, "JWT 형식이 잘못됨"),
+    INVALID_TOKEN(401, 9006, "유효하지 않은 JWT 토큰"),
+    INVALID_TOKEN_TYPE(400,9007,"적절한 토큰 타입이 아닙니다(ACCESS,REFRESH)"),
+    NOTFOUND_TOKEN_IN_COOKIE(404,9008,"쿠키에서 토큰값을 찾을 수 없습니다"),
+    NOTFOUND_USER(404,9009,"토큰 페이로드 정보로 찾을 수 없는 사용자 입니다.")
     ;
 
     private final int httpStatusCode;
