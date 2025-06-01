bd7f005270de9935e6d7bb463c3057609acc57ef
JoonHyuk0331 <joonlife0901@gmail.com>
2025-06-01 23:59:16 +0900

    fix(Constants): 엔드포인트 미포함 버그 해결

--- src/main/java/com/matchday/matchdayserver/common/Constants.java
+++ src/main/java/com/matchday/matchdayserver/common/Constants.java
@@ -3,8 +3,9 @@ package com.matchday.matchdayserver.common;
 public class Constants {
 
   public static final String FRONTEND_LOCAL_URL = "http://localhost:5173";
+  public static final String FRONTEND_PRODUCTION_URL = "https://matchday-planner.com/";
+  public static final String FRONTEND_DEV_URL = "https://dev.matchday-planner.com/";
   public static final String BACKEND_LOCAL_URL = "http://localhost:8080"; //swagger
-  public static final String FRONTEND_PRODUCTION_URL = "https://match-day.netlify.app";
   public static final String BACKEND_PRODUCTION_URL = "https://dev-api.matchday-planner.com";
   public static final String FRONTEND_LOCAL_VUE_URL = "http://localhost:3000";
 
