b640a8e4f3caf60df4f7281d53a8963b89bcc3af
JoonHyuk0331 <joonlife0901@gmail.com>
2025-06-08 13:11:09 +0900

    feat(UserController): 엔드포인트 권한 추가

--- src/main/java/com/matchday/matchdayserver/user/controller/UserController.java
+++ src/main/java/com/matchday/matchdayserver/user/controller/UserController.java
@@ -29,7 +29,14 @@ public class UserController {
     private static final String FOLDER_NAME = "users";
 
     @ResponseStatus(HttpStatus.CREATED)
-    @Operation(summary = "임시 유저 생성", description = "생성된 user의 Id 값 반환 <br> profileImg는 선택 사항입니다.")
+    @Operation(
+        summary = "임시 유저 생성",
+        description = """
+            생성된 user의 Id 값 반환  
+            profileImg는 선택 사항입니다.  
+            [필요 권한] ADMIN
+            """
+    )
     @PostMapping
     @PreAuthorize("hasRole('ADMIN')")
     public ApiResponse<Long> createUser(@Valid @RequestBody UserCreateRequest request) {
@@ -37,16 +44,26 @@ public class UserController {
         return ApiResponse.created(userId);
     }
 
-    @Operation(summary = "팀 입단", description = "{userId}가 teamId에 소속됩니다")
+    @Operation(
+        summary = "팀 입단",
+        description = """
+            {userId}가 teamId에 소속됩니다  
+            [필요 권한] USER
+            """
+    )
     @PostMapping("/{userId}/teams")
-    @PreAuthorize("hasRole('ADMIN')")
+    @PreAuthorize("hasRole('USER')")
     public ApiResponse<?> joinTeam(@PathVariable Long userId, @RequestBody UserJoinTeamRequest request) {
         return ApiResponse.ok(userService.joinTeam(userId, request));
     }
 
     @Operation(
         summary = "유저 프로필 업로드 presigned URL 발급",
-        description = "userid와 확장자(jpg, jpeg, png, webp)를 넘겨주면 이미지 업로드용 URL과 저장할 FileName(파일명)을 반환합니다. <br> 반환 받은 파일명을 저장하세요(이미지 조회시 필요)"
+        description = """
+            userid와 확장자(jpg, jpeg, png, webp)를 넘겨주면  
+            이미지 업로드용 URL과 저장할 FileName(파일명)을 반환합니다.  
+            반환 받은 파일명을 저장하세요 (이미지 조회시 필요)
+            """
     )
     @GetMapping("/{userId}/profile/upload-url")
     public ApiResponse<S3PresignedResponse> generateUploadUrl(
@@ -58,7 +75,10 @@ public class UserController {
 
     @Operation(
         summary = "유저 프로필 이미지 read presigned URL 조회",
-        description = "user 프로필 업로드에서 반환 받은 파일명(key)을 넘겨주면 이미지 read용 URL을 반환합니다"
+        description = """
+            user 프로필 업로드에서 반환 받은 파일명(key)을 넘겨주면  
+            이미지 read용 URL을 반환합니다
+            """
     )
     @GetMapping("/{userId}/profile/read-url")
     public ApiResponse<S3PresignedResponse> getProfileReadUrl(
@@ -70,25 +90,42 @@ public class UserController {
 
     @Operation(
         summary = "유저 정보 조회",
-        description = "유저 정보 조회 API입니다. <br> 특정 유저의 이름, 소속팀 id, 참여한 매치 id를 반환합니다."
+        description = """
+            유저 정보 조회 API입니다.  
+            특정 유저의 이름, 소속팀 id, 참여한 매치 id를 반환합니다.  
+            [필요 권한] ADMIN
+            """
     )
+    @PreAuthorize("hasRole('ADMIN')")
     @GetMapping("/{userId}")
     public ApiResponse<UserInfoResponse> getUserInfo(@PathVariable Long userId) {
         return ApiResponse.ok(userService.getUserInfo(userId));
     }
 
-    @Operation(summary = "로그아웃", description = "현재 로그인된 사용자의 리프레시 토큰을 사용할 수 없게 합니다")
+    @Operation(
+        summary = "로그아웃",
+        description = """
+            현재 로그인된 사용자의 리프레시 토큰을 사용할 수 없게 합니다
+            """
+    )
     @PostMapping("/logout")
     public ApiResponse<Long> logout(@UserId Long userId) {
         userService.logout(userId);
         return ApiResponse.ok(userId);
     }
 
-    @Operation(summary = "유저 권한 부여", description = "지정한 유저의 권한을 변경합니다")
+    @Operation(
+        summary = "유저 권한 부여",
+        description = """
+            지정한 유저의 권한을 변경합니다  
+            [필요 권한] SUPER_ADMIN
+            """
+    )
     @PreAuthorize("hasRole('ROLE_SUPER_ADMIN')")
     @PostMapping("/roles")
     public ApiResponse<UpdateUserRoleResponse> updateRole(@RequestBody @Valid UpdateUserRoleRequest request) {
-        UpdateUserRoleResponse response= userService.updateUserRole(request);
+        UpdateUserRoleResponse response = userService.updateUserRole(request);
         return ApiResponse.ok(response);
     }
 }
+
