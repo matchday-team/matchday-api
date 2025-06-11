42a38f47a4f25c7cb136ffce8276713b139a30b4
JoonHyuk0331 <joonlife0901@gmail.com>
2025-06-11 22:17:57 +0900

    feat(OauthloginRequest): redirectUri 필드 추가

--- src/main/java/com/matchday/matchdayserver/auth/model/dto/request/OauthLoginRequest.java
+++ src/main/java/com/matchday/matchdayserver/auth/model/dto/request/OauthLoginRequest.java
@@ -10,6 +10,8 @@ import lombok.Setter;
 @NoArgsConstructor
 public class OauthLoginRequest {
     private String code;
+    private String redirectUri; //
     //여기에 프론트에서 폼 등으로 입력받은 추가데이터 받을 수 있음
+    //kakao,goole 로그인 둘다 이거로 쓰는중
     //ex 선호 팀,나이 등 oauth 회원가입
 }
