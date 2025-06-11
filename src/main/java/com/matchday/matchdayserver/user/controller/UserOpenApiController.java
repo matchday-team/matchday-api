deccfc5072454579c517b7e53f31f283360c9547
JoonHyuk0331 <joonlife0901@gmail.com>
2025-06-11 22:19:50 +0900

    feat(UserOpenApi): kakao 로그인 엔드포인트 추가

--- src/main/java/com/matchday/matchdayserver/user/controller/UserOpenApiController.java
+++ src/main/java/com/matchday/matchdayserver/user/controller/UserOpenApiController.java
@@ -5,10 +5,10 @@ import com.matchday.matchdayserver.auth.model.dto.response.OauthLoginResponse;
 import com.matchday.matchdayserver.auth.model.dto.response.RenewResponse;
 import com.matchday.matchdayserver.auth.model.dto.response.TokenRenewResponse;
 import com.matchday.matchdayserver.auth.service.GoogleOauthService;
+import com.matchday.matchdayserver.auth.service.KakaoOauthService;
 import com.matchday.matchdayserver.common.response.ApiResponse;
 import com.matchday.matchdayserver.user.model.dto.LoginUserDto;
 import com.matchday.matchdayserver.user.model.dto.response.LoginResponse;
-import com.matchday.matchdayserver.user.model.entity.User;
 import com.matchday.matchdayserver.user.service.UserOpenApiService;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.servlet.http.HttpServletRequest;
@@ -28,12 +28,25 @@ import org.springframework.web.bind.annotation.RestController;
 public class UserOpenApiController {
 
     private final GoogleOauthService googleOauthService;
+    private final KakaoOauthService kakaoOauthService;
     private final UserOpenApiService userOpenApiService;
 
     @PostMapping("/google")
     public ApiResponse<OauthLoginResponse> googleLogin(@RequestBody OauthLoginRequest oAuthLoginRequest,HttpServletResponse response){
         //유저 정보 가져오기
         LoginUserDto loginUserDto = googleOauthService.googleLogin(oAuthLoginRequest);
+        //유저 정보로 엑세스토큰과 리프레시 토큰 받아오기
+        LoginResponse loginResponse = userOpenApiService.doLogin(loginUserDto);
+        //쿠키 세팅
+        response.addCookie(userOpenApiService.createRefreshCookie(loginResponse.getRefreshToken()));
+        //엑세스 토큰 리턴
+        return ApiResponse.ok(new OauthLoginResponse(loginResponse.getAccessToken()));
+    }
+
+    @PostMapping("/kakao")
+    public ApiResponse<OauthLoginResponse> kakaoLogin(@RequestBody OauthLoginRequest oAuthLoginRequest,HttpServletResponse response){
+        //유저 정보 가져오기
+        LoginUserDto loginUserDto = kakaoOauthService.kakaoLogin(oAuthLoginRequest);
         //유저 정보로 엑세스토큰과 리프레시 토큰 받아오기
         LoginResponse loginResponse = userOpenApiService.doLogin(loginUserDto);
         //쿠키 세팅
