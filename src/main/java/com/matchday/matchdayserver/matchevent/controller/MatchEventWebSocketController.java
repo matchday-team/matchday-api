29c3cefc1b0fabeb2420ebee085e64a123e200b2
roky <rookedsysc36@gmail.com>
2025-06-14 02:35:49 +0900

    feat: MatchEventWebSocketController 구현

--- src/main/java/com/matchday/matchdayserver/matchevent/controller/MatchEventWebSocketController.java
+++ src/main/java/com/matchday/matchdayserver/matchevent/controller/MatchEventWebSocketController.java
@@ -5,14 +5,16 @@ import com.matchday.matchdayserver.matchevent.model.dto.MatchEventRequest;
 import com.matchday.matchdayserver.matchevent.model.dto.MatchEventUserRequest;
 import com.matchday.matchdayserver.matchevent.service.MatchEventCancelService;
 import com.matchday.matchdayserver.matchevent.service.MatchEventSaveService;
+import com.matchday.matchdayserver.common.websocket.resolver.WebSocketUserId;
 
 import org.springframework.messaging.handler.annotation.DestinationVariable;
 import org.springframework.messaging.handler.annotation.MessageMapping;
 import org.springframework.stereotype.Controller;
 
-
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @Controller
 @RequiredArgsConstructor
 public class MatchEventWebSocketController {
@@ -22,24 +24,32 @@ public class MatchEventWebSocketController {
 
     @MessageMapping("/match/{matchId}")
     public void recordEvent(@DestinationVariable Long matchId,
-        MatchEventUserRequest matchEventRequest) {
-
+                           @WebSocketUserId Long userId,
+                           MatchEventUserRequest matchEventRequest) {
+        
+        log.info("Recording event for match: {}, user: {}", matchId, userId);
+        // userId는 JWT 토큰에서 자동으로 추출되어 주입됩니다
         matchEventSaveService.saveMatchEvent(matchId, matchEventRequest);
     }
 
     @MessageMapping("/match/{matchId}/teams/{teamId}")
-    public void recordTeamEvent(
-        @DestinationVariable Long matchId,
-        @DestinationVariable Long teamId,
-        MatchEventRequest matchEventRequest) {
-
+    public void recordTeamEvent(@DestinationVariable Long matchId,
+                               @DestinationVariable Long teamId,
+                               @WebSocketUserId Long userId,
+                               MatchEventRequest matchEventRequest) {
+        
+        log.info("Recording team event for match: {}, team: {}, user: {}", matchId, teamId, userId);
+        // userId는 JWT 토큰에서 자동으로 추출되어 주입됩니다
         matchEventSaveService.saveMatchTeamEvent(matchId, teamId, matchEventRequest);
     }
 
     @MessageMapping("/match/{matchId}/cancel")
-    public void cancelEvent(
-        @DestinationVariable Long matchId,
-        MatchEventCancelRequest matchEventCancelRequest) {
+    public void cancelEvent(@DestinationVariable Long matchId,
+                           @WebSocketUserId Long userId,
+                           MatchEventCancelRequest matchEventCancelRequest) {
+        
+        log.info("Canceling event for match: {}, user: {}", matchId, userId);
+        // userId는 JWT 토큰에서 자동으로 추출되어 주입됩니다
         matchEventCancelService.cancelEvent(matchId, matchEventCancelRequest);
     }
 }
