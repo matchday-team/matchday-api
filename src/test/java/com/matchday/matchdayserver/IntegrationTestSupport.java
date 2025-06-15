4f27d3e854980135f63e1efad5d86a0e5a2aaa50
roky <rookedsysc36@gmail.com>
2025-06-15 19:49:51 +0900

    feat(IntegrationTestSupport): ActiveProfiles test로 지정

--- src/test/java/com/matchday/matchdayserver/IntegrationTestSupport.java
+++ src/test/java/com/matchday/matchdayserver/IntegrationTestSupport.java
@@ -1,10 +1,14 @@
 package com.matchday.matchdayserver;
 
+import jakarta.transaction.Transactional;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.TestPropertySource;
 
+@Transactional
 @AutoConfigureMockMvc
+@ActiveProfiles("test")
 @TestPropertySource(locations = "classpath:application-test.yml")
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class IntegrationTestSupport {
